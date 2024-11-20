60c396075318d2590b9b607d5542e83c6eb7e260
Admin <Admin@DESKTOP-QNTLQCC>
2024-11-20 15:54:53 +0300

    Ülesane 1 oli tehtud.

--- Hello_world.py
+++ Hello_world.py
@@ -1,2 +1,15 @@
+#Ülesane 1
 print("Tere tulemast!")
+nimi=input("Mis on sinu nimi? ")
+print("Tere tulemast! Tervitan sind ", nimi)
+print("Tere tulemast! Tervitan sind "+ nimi)
+vanus=int(input("Kui vana sa oled? "))
+print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastad vana")
+print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastad vana")
 
+#Ülesane 2
+vanus = 18
+eesnimi = "zenja"
+pikkus = 16,5
+kas_käib_koolis = True
+print(type(vanus))
