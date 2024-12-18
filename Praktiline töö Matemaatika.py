14490d58776f6b86d7bb731a8aef6d1e097bbdfc
Admin <Admin@DESKTOP-QNTLQCC>
2024-12-18 14:03:11 +0200

    Praktiline töö: "Matemaatika" исправлена

--- Praktiline töö Matemaatika.py
+++ Praktiline töö Matemaatika.py
@@ -3,35 +3,36 @@ raskus = int(input("Введите 1, 2 или 3: "))
 õvastus = 0  
 kui_palju_näiteid = 10  
 for i in range(1, kui_palju_näiteid + 1):
-    a = 0
-    b = 0
+    a = i + 1  
+    b = i + 2
     kasutame = ""
     õige_vastus = 0  
     if raskus == 1:
-        a = i + 1
-        b = i + 2
         õige_vastus = a + b
         kasutame = "+"
     elif raskus == 2:
-        a = i * 2
-        b = i
-        õige_vastus = a - b
-        kasutame = "-"
+        if i % 2 == 0:  
+            õige_vastus = a - b
+            kasutame = "-"
+        else:
+            b = i  
+            õige_vastus = a // b 
+            kasutame = "/"
     elif raskus == 3:
-        a = i + 2
-        b = i
-        õige_vastus = a * b
-        kasutame = "*"
+        if i % 2 == 0:  
+            õige_vastus = a * b
+            kasutame = "*"
+        else:
+            õige_vastus = a ** 2
+            kasutame = "**"
     else:
         print("Ошибка! Введите 1, 2 или 3.")
         break
-    print(f"Пример {i}: {a} {kasutame} {b} = ?")
+    print(f"Пример {i}: {a} {kasutame} {b if kasutame != '**' else ''} = ?")
     teie_vastus = int(input("Ваш ответ: "))
     if teie_vastus == õige_vastus:
         print("Правильно!")
         õvastus += 1
-    else:
-        print(f"Ошибка! Правильный ответ: {õige_vastus}")
 hinde_protsent = (õvastus / kui_palju_näiteid) * 100
 if hinde_protsent < 60:
     hinne = "Hinne 2"
