d93bed993f8e0d46b1f8cfaba89943d904bae981
Admin <Admin@DESKTOP-QNTLQCC>
2025-02-05 11:44:29 +0200

    Iseseisevtöö "Registreerimine ja autoriseerimine"

--- MyModule.py
+++ MyModule.py
@@ -1,55 +1,80 @@
 import random
 import string
 
-# Alguses täidetud kasutajate ja paroolide nimekirjad
 kasutajad = ["kasutaja1", "kasutaja2"]
 paroolid = ["Parool123!", "Salasona456*"]
 
-def kasutaja_haldus(tegevus):
-    if tegevus == "registreeri":
-        nimi = input("Sisesta kasutajanimi: ")
-        if nimi in kasutajanimed:
-            print("Selline kasutajanimi on juba olemas!")
-            return
-        parool = input("Sisesta parool või jäta tühjaks automaatseks genereerimiseks: ")
-        if not parool:
-            parool = ''.join(random.choices(string.ascii_letters + string.digits + "!@#$%^&*", k=12))
-            print(f"Sinu genereeritud parool: {parool}")
-        kasutajanimed.append(nimi)
-        paroolid.append(parool)
-        print("Registreerimine õnnestus!")
-    
-    elif tegevus == "autoriseeri":
-        nimi = input("Sisesta kasutajanimi: ")
-        if nimi in kasutajanimed:
-            indeks = kasutajanimed.index(nimi)
-            if paroolid[indeks] == input("Sisesta parool: "):
-                print("Sisselogimine õnnestus!")
-            else:
-                print("Vale parool!")
-        else:
-            print("Sellist kasutajat ei ole!")
-    
-    elif tegevus == "muuda_parooli":
-        nimi = input("Sisesta kasutajanimi: ")
-        if nimi in kasutajanimed:
-            indeks = kasutajanimed.index(nimi)
-            vana = input("Sisesta vana parool: ")
-            if paroolid[indeks] == vana:
-                uus_parool = input("Sisesta uus parool: ")
-                paroolid[indeks] = uus_parool
-                print("Parool on muudetud!")
-            else:
-                print("Vale parool!")
-        else:
-            print("Sellist kasutajat ei ole!")
-    
-    elif tegevus == "taasta_parool":
-        nimi = input("Sisesta kasutajanimi: ")
-        if nimi in kasutajanimed:
-            indeks = kasutajanimed.index(nimi)
-            uus_parool = ''.join(random.choices(string.ascii_letters + string.digits + "!@#$%^&*", k=12))
-            paroolid[indeks] = uus_parool
-            print(f"Sinu uus parool on: {uus_parool}")
-        else:
-            print("Sellist kasutajat ei ole!")
+def loo_parool():
+    tegelased = string.ascii_letters + string.digits + ".,:;!_*-+()/#¤%&"
+    parool = ''.join(random.choice(tegelased) for _ in range(12))
+    return parool
+
+def kontrolli_parooli(parool):
+    return (any(c.isdigit() for c in parool) and
+            any(c.islower() for c in parool) and
+            any(c.isupper() for c in parool) and
+            any(c in ".,:;!_*-+()/#¤%&" for c in parool))
+
+def registreeri():
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    if kasutajanimi in kasutajad:
+        print("Kasutajanimi on juba olemas!")
+        return
+
+    valik = input("Kas soovid automaatset parooli? (jah/ei): ").lower()
+    if valik == "jah":
+        parool = loo_parool()
+        print(f"Sinu parool: {parool}")
+    else:
+        while True:
+            parool = input("Sisesta parool: ")
+            if kontrolli_parooli(parool):
+                break
+            print("Parool peab sisaldama suurtähti, väiketähti, numbreid ja erisümboleid!")
+
+    kasutajad.append(kasutajanimi)
+    paroolid.append(parool)
+    print("Registreerimine õnnestus!")
+
+def autoriseeri():
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    if kasutajanimi not in kasutajad:
+        print("Kasutajat ei leitud!")
+        return
+
+    parool = input("Sisesta parool: ")
+    if paroolid[kasutajad.index(kasutajanimi)] == parool:
+        print("Sisselogimine õnnestus!")
+    else:
+        print("Vale parool!")
+
+def muuda_andmeid():
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    if kasutajanimi not in kasutajad:
+        print("Kasutajat ei leitud!")
+        return
+
+    indeks = kasutajad.index(kasutajanimi)
+    uus_kasutajanimi = input("Sisesta uus kasutajanimi (jäta tühjaks, kui ei soovi muuta): ")
+    if uus_kasutajanimi and uus_kasutajanimi not in kasutajad:
+        kasutajad[indeks] = uus_kasutajanimi
+        print("Kasutajanimi edukalt muudetud!")
+
+    uus_parool = input("Sisesta uus parool (jäta tühjaks, kui ei soovi muuta): ")
+    if uus_parool:
+        while not kontrolli_parooli(uus_parool):
+            print("Parool peab sisaldama suurtähti, väiketähti, numbreid ja erisümboleid!")
+            uus_parool = input("Sisesta uus parool: ")
+        paroolid[indeks] = uus_parool
+        print("Parool edukalt muudetud!")
+
+def taasta_parool():
+    kasutajanimi = input("Sisesta kasutajanimi: ")
+    if kasutajanimi not in kasutajad:
+        print("Kasutajat ei leitud!")
+        return
+
+    indeks = kasutajad.index(kasutajanimi)
+    uus_parool = loo_parool()
+    paroolid[indeks] = uus_parool
+    print(f"Sinu uus parool: {uus_parool}")
--- põhiprogrammi.py
+++ põhiprogrammi.py
@@ -1,23 +1,27 @@
 import MyModule
 
-while True:
-    print("\nVali tegevus:")
-    print("1 - Registreerimine")
-    print("2 - Autoriseerimine")
-    print("3 - Parooli muutmine")
-    print("4 - Unustatud parooli taastamine")
-    print("5 - Lõpetamine")
-    valik = input("Sisesta valik: ")
-    if valik == "1":
-        MyModule.kasutaja_haldus("registreeri")
-    elif valik == "2":
-        MyModule.kasutaja_haldus("autoriseeri")
-    elif valik == "3":
-        MyModule.kasutaja_haldus("muuda_parooli")
-    elif valik == "4":
-        MyModule.kasutaja_haldus("taasta_parool")
-    elif valik == "5":
-        print("Programmi lõpetamine...")
-        break
-    else:
-        print("Vale valik, proovi uuesti!")
+if __name__ == "__main__":
+
+    while True:
+        print("\nVali tegevus:")
+        print("1. Registreerimine")
+        print("2. Autoriseerimine")
+        print("3. Muuda nime või parooli")
+        print("4. Taasta parool")
+        print("5. Lõpeta")
+
+        valik = input("Sisesta valik (1-5): ")
+
+        if valik == '1':
+            MyModule.registreeri()
+        elif valik == '2':
+            MyModule.autoriseeri()
+        elif valik == '3':
+            MyModule.muuda_andmeid()
+        elif valik == '4':
+            MyModule.taasta_parool()
+        elif valik == '5':
+            print("Programmi lõpetamine.")
+            break
+        else:
+            print("Vale valik! Proovi uuesti.")
