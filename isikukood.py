f2b6362eb19062e6ebd64261cc27e057980a8518
Admin <Admin@DESKTOP-QNTLQCC>
2025-02-05 11:18:33 +0200

    isikukood parandamine

--- isikukood.py
+++ isikukood.py
@@ -1,159 +1,87 @@
-# isikukood ilma def
-ikoodid = []
-arvud = []
+ikoodid = [] 
+arvud = [] 
+
+haiglad = {
+    range(1, 11): "Kuressaare Haigla",
+    range(11, 20): "Tartu Ülikooli Naistekliinik, Tartumaa, Tartu",
+    range(21, 221): "Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla",
+    range(221, 271): "Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)",
+    range(271, 371): "Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla", 
+    range(371, 421): "Narva Haigla",
+    range(421, 471): "Pärnu Haigla",
+    range(471, 491): "Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla",
+    range(491, 521): "Järvamaa Haigla (Paide)",
+    range(521, 571): "Rakvere, Tapa haigla",
+    range(571, 601): "Valga Haigla",
+    range(601, 651): "Viljandi Haigla",
+    range(651, 701): "Lõuna-Eesti Haigla (Võru), Põlva Haigla"
+}
+def kontrollnumber(code):  
+    astme_kaal1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+    astme_kaal2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+
+    sum1 = sum(int(code[i]) * astme_kaal1[i] for i in range(10))
+    ule_osa1 = sum1 % 11
+
+    if ule_osa1 != 10:
+        return ule_osa1
+
+    sum2 = sum(int(code[i]) * astme_kaal2[i] for i in range(10))
+    ule_osa2 = sum2 % 11
+
+    return ule_osa2 if ule_osa2 != 10 else 0
+
+def hoiused(number):
+    for range_key, place in haiglad.items():
+        if number in range_key:
+            return place
+    return "Tundmatu koht"
+
+def isikukood(code): 
+    if len(code) != 11 or not code.isdigit():
+        return False
+
+    if int(code[0]) not in range(1, 7):
+        return False
+
+    aastal = 1800 + ((int(code[0]) - 1) // 2) * 100
+    year = aastal + int(code[1:3])
+    month = int(code[3:5])
+    day = int(code[5:7])
+
+    try:
+        from datetime import datetime
+        datetime(year, month, day)
+    except ValueError:
+        return False
+
+    lnumber = int(code[7:10])
+    control = kontrollnumber(code)
+
+    return control == int(code[10])
 
 while True:
-    kood = input("Sisesta isikukood (või STOP): ")
-    if kood == "STOP":
+    code = input("Sisestage isikukood või 'STOP' lõpetamiseks: ")
+    if code.strip().upper() == "STOP":
         break
-    if len(kood) != 11 or not kood.isdigit():
-        print("Vale pikkus.")
-        arvud = arvud + [kood]  
-    elif kood[0] not in "123456":
-        print("Esimene number on vale.")
-        arvud = arvud + [kood]
+
+    if isikukood(code):
+        gender = "naine" if int(code[0]) % 2 == 0 else "mees"
+        aastal = 1800 + ((int(code[0]) - 1) // 2) * 100
+        year = aastal + int(code[1:3])
+        month = int(code[3:5])
+        day = int(code[5:7])
+        lnumber = int(code[7:10])
+        place = hoiused(lnumber)
+
+        ikoodid.append(code)
+        print(f"See on {gender}, tema sünnipäev on {day:02}.{month:02}.{year}, sünnikoht on {place}.")
     else:
-        kaal1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
-        kaal2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-        summa1 = 0
-        for i in range(10):
-            summa1 += int(kood[i]) * kaal1[i]
-        jaak = summa1 % 11
-        if jaak == 10:
-            summa2 = 0
-            for i in range(10):
-                summa2 += int(kood[i]) * kaal2[i]
-            jaak = summa2 % 11
-        if jaak == 10:
-            jaak = 0
-        if jaak != int(kood[-1]):
-            print("Isiskukood on vale.")
-            arvud = arvud + [kood]
-        else:
-            aasta_algus = {"1": "18", "2": "18", "3": "19", "4": "19", "5": "20", "6": "20"}
-            aasta = aasta_algus[kood[0]] + kood[1:3]
-            kuu = kood[3:5]
-            paev = kood[5:7]
-            jarjekord = int(kood[7:10])
-            if jarjekord <= 10:
-                haigla = "Kuressaare Haigla"
-            elif jarjekord <= 19:
-                haigla = "Tartu Ülikooli Naistekliinik"
-            elif jarjekord <= 220:
-                haigla = "Ida-Tallinna Keskhaigla"
-            elif jarjekord <= 270:
-                haigla = "Ida-Viru Keskhaigla"
-            elif jarjekord <= 370:
-                haigla = "Maarjamõisa Kliinikum"
-            elif jarjekord <= 420:
-                haigla = "Narva Haigla"
-            elif jarjekord <= 470:
-                haigla = "Pärnu Haigla"
-            elif jarjekord <= 490:
-                haigla = "Pelgulinna Sünnitusmaja"
-            elif jarjekord <= 520:
-                haigla = "Järvamaa Haigla"
-            elif jarjekord <= 570:
-                haigla = "Rakvere, Tapa haigla"
-            elif jarjekord <= 600:
-                haigla = "Valga Haigla"
-            elif jarjekord <= 650:
-                haigla = "Viljandi Haigla"
-            elif jarjekord <= 700:
-                haigla = "Lõuna-Eesti Haigla"
-            else:
-                haigla = "Tundmatu haigla"
-            sugu = "mees" if kood[0] in "135" else "naine"
-            synnikuupaev = paev + "." + kuu + "." + aasta
-            ikoodid = ikoodid + [(kood, sugu)]  
-            print("See on", sugu, ", sünniaeg", synnikuupaev, ", haigla", haigla)
-for i in range(len(arvud)):
-    for j in range(len(arvud) - i - 1):
-        if arvud[j] > arvud[j + 1]:
-            arvud[j], arvud[j + 1] = arvud[j + 1], arvud[j]
-ikoodid_naised = []
-ikoodid_mehed = []
-for kood, sugu in ikoodid:
-    if sugu == "naine":
-        ikoodid_naised = ikoodid_naised + [kood]
-    else:
-        ikoodid_mehed = ikoodid_mehed + [kood]
-print("\nÕiged koodid:")
-for kood in ikoodid_naised + ikoodid_mehed:
-    print(kood)
-print("\nValed koodid:")
-for kood in arvud:
-    print(kood)
+        arvud.append(code)
+        print("Vigane isikukood.")
 
-# isikukood def
-def töötle_kood(kood):
-    """
-    :param str kood: Личный код (isikukood), который нужно проверить. Должен состоять из 11 цифр.
-    :rtype: Если код корректный, то указывается (пол, дата рождения, место рождения). Если код некорректный, возвращается строка с описанием ошибки.
-    """
-    kood = str(kood)
+    ikoodid_sorted = sorted(ikoodid)
+    arvud_sorted = sorted(arvud)
 
-    if len(kood) != 11 or not kood.isdigit():
-        vastus = "Vale pikkus."
-    elif kood[0] not in "123456":
-        vastus = "Esimene number on vale."
-    else:
-        kaalud1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
-        kaalud2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-
-        summa1 = sum(int(kood[i]) * kaalud1[i] for i in range(10))
-        jaak = summa1 % 11
-
-        if jaak == 10:
-            summa2 = sum(int(kood[i]) * kaalud2[i] for i in range(10))
-            jaak = summa2 % 11
-
-        isikukood = 0 if jaak == 10 else jaak
-
-        if isikukood != int(kood[-1]):
-            vastus = "Isikukood on vale."
-        else:
-            algusaasta = {"1": "18", "2": "18", "3": "19", "4": "19", "5": "20", "6": "20"}
-            aasta = algusaasta[kood[0]] + kood[1:3]
-            kuu = kood[3:5]
-            paev = kood[5:7]
-
-            synnikuupaev = (f"{paev}.{kuu}.{aasta}")
-
-            sugu = "mees" if kood[0] in "135" else "naine"
-
-            jarjekord = int(kood[7:10])
-            if jarjekord <= 10:
-                haigla = "Kuressaare Haigla"
-            elif jarjekord <= 19:
-                haigla = "Tartu Ülikooli Naistekliinik"
-            elif jarjekord <= 220:
-                haigla = "Ida-Tallinna Keskhaigla"
-            elif jarjekord <= 270:
-                haigla = "Ida-Viru Keskhaigla"
-            elif jarjekord <= 370:
-                haigla = "Maarjamõisa Kliinikum"
-            elif jarjekord <= 420:
-                haigla = "Narva Haigla"
-            elif jarjekord <= 470:
-                haigla = "Pärnu Haigla"
-            elif jarjekord <= 490:
-                haigла = "Pelgulinna Sünnitusmaja"
-            elif jarjekord <= 520:
-                haigla = "Järvamaa Haigla"
-            elif jarjekord <= 570:
-                haigла = "Rakvere, Tapa haigla"
-            elif jarjekord <= 600:
-                haigла = "Valga Haigla"
-            elif jarjekord <= 650:
-                haigла = "Viljandi Haigla"
-            elif jarjekord <= 700:
-                haigла = "Lõuna-Eesti Haigла"
-            else:
-                haigла = "Tundmatu haigла"
-
-            vastus = (sugu, synnikuupaev, haigla)
-
-    return vastus
-
-
+    print("\nKorrektseid koode:", ikoodid_sorted)
+    print("Vigaseid koode:", arvud_sorted)
